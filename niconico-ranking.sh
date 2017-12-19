3d14324c5bebc41007e2b61e60f429dfc142c2e8
tatsunori <33901217+ObayashiTatsunori@users.noreply.github.com>
2017-12-19 22:03:47 +0900

    ニコニコ動画のランキングを取得するシェル

---
+++ niconico-ranking.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dirname="/home/ubuntu/workspace/niconico-ranking-rss"
+mkdir -p $dirname
+filename="${dirname}/hourly-ranking-`date +'%Y%m%d%H%M'`.xml"
+echo "Save to $filename"
+
+curl -s -o $filename -H "User-Agent: CrawlBot; 4210.tatu@gmail.com" http://www.nicovideo.jp/ranking/fav/hourly/all?rss=2.0&lang=ja-jp 
